4849dca60121c441aa8cdb4863c1f1963de84c0c
thailehoangbao <thaibao29111994@gmail.com>
2024-06-17 20:32:34 +0700

    update logo gg fb

--- resources/views/client/users/loginggfb.blade.php
+++ resources/views/client/users/loginggfb.blade.php
@@ -1,11 +1,11 @@
 
 <div class="d-flex justify-content-between" style="padding-top: 12px;">
     <a class="btn btn-outline-blue w-100 me-4 py-3" href="{{url('client/google')}}" style="border: 1px solid #888;background-color: #f3f3f3;border-radius: 8px;padding: 8px; cursor: pointer; margin: 2px;">
-        <img src="http://123.30.149.20/img/google-icon.png" class="icon" width="20">
+        <img src="/images/google_logo.png" class="icon" width="20">
         <span class="text">Google</span>
     </a>
     <a class="btn btn-outline-blue w-100 ms-4 py-3" href="" style="border: 1px solid #888;background-color: #f3f3f3;border-radius: 8px;padding: 8px; cursor: pointer; margin: 2px;">
-        <img src="http://123.30.149.20/img/facebook-icon.png" class="icon" width="20">
+        <img src="/images/fb_logo.svg" class="icon" width="20">
         <span class="text">Facebook</span>
     </a>
 </div>
